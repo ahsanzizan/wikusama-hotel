cd37a732a23358b51aebbe1feb941b48f468790b
Ahsan <ahsanaz461@gmail.com>
2024-09-22 12:17:29 +0700

    feat: Use raleway as the main font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import NextAuthProvider from "@/components/providers/NextAuthProvider";
 import ProgressBarProvider from "@/components/providers/ProgressBarProvider";
 import ToasterProvider from "@/components/providers/ToasterProvider";
 import type { Metadata, Viewport } from "next";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,6 +16,11 @@ export const viewport: Viewport = {
   initialScale: 1,
 };
 
+const ralewayFont = Raleway({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -22,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`${ralewayFont.className} antialiased`}>
         <NextAuthProvider>
           <ToasterProvider />
           <ProgressBarProvider />
