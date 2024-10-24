81cedecd14cae30b226b364e7ab62359af041cba
Ahsan <ahsanaz461@gmail.com>
2024-10-24 09:48:27 +0700

    fix: Put the downloadPdf in the useEffect

--- src/app/bookings/receipt/components/ReceiptContainer.tsx
+++ src/app/bookings/receipt/components/ReceiptContainer.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { Prisma } from "@prisma/client";
 import html2pdf from "html2pdf.js";
-import { useCallback, useEffect, useRef } from "react";
+import { useRouter } from "next-nprogress-bar";
+import { useEffect, useRef } from "react";
 import DownloadableReceipt from "./DownloadableReceipt";
 
 interface ReceiptContainerProps {
@@ -17,35 +18,36 @@ export default function ReceiptContainer({
   bookingReceipt,
 }: ReceiptContainerProps) {
   const receiptRef = useRef<HTMLDivElement>(null);
-
-  const downloadPdf = useCallback(() => {
-    const options = {
-      margin: 0,
-      filename: `Receipt_${bookingReceipt.user.name}_Room${bookingReceipt.booking.room.room_number}.pdf`,
-      image: { type: "jpeg", quality: 1 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-    };
-    const element = receiptRef.current;
-
-    if (element) {
-      html2pdf()
-        .from(element)
-        .set(options)
-        .save(
-          `Receipt_${bookingReceipt.user.name}_Room${bookingReceipt.booking.room.room_number}.pdf`,
-        )
-        .catch((err: string) => console.error(err))
-        .then(() => {
-          window.location.href = "/bookings";
-        });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const router = useRouter();
 
   useEffect(() => {
+    const downloadPdf = async () => {
+      const options = {
+        margin: 0,
+        filename: `Receipt_${bookingReceipt.user.name}_Room${bookingReceipt.booking.room.room_number}.pdf`,
+        image: { type: "jpeg", quality: 1 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+      };
+      const element = receiptRef.current;
+
+      if (element) {
+        await html2pdf()
+          .from(element)
+          .set(options)
+          .save(
+            `Receipt_${bookingReceipt.user.name}_Room${bookingReceipt.booking.room.room_number}.pdf`,
+          )
+          .catch((err: string) => console.error(err))
+          .then(() => {
+            router.push("/bookings");
+          });
+      }
+    };
+
     downloadPdf();
-  }, [downloadPdf]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <DownloadableReceipt bookingReceipt={bookingReceipt} ref={receiptRef} />
